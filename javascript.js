a4a8b78ea8ddabe7c55adafdb4ac6f9426fb4988
Spac3y <73653851+Spac3y@users.noreply.github.com>
2023-05-26 19:26:36 +0300

    Change bg color on hover and show what icon is it

--- javascript.js
+++ javascript.js
@@ -2,9 +2,47 @@ const main = document.getElementById("main").children;
 
 fb =main[0];
 insta = main[1];
-tt = main[2];
+git = main[2];
 wp = main[3];
 
 fb.addEventListener("mouseover", function() {
-	document.getElementsByTagName("body").style.color = "blue";
+	document.getElementsByTagName("body")[0].style.backgroundColor = "#4267B2";
+	document.getElementById("text-indicator").innerHTML = "Facebook";
 })
+
+fb.addEventListener("mouseout", function() {
+	document.getElementsByTagName("body")[0].style.backgroundColor = "#e2eee4";
+	document.getElementById("text-indicator").innerHTML = "";
+})
+
+insta.addEventListener("mouseover", function() {
+	document.getElementsByTagName("body")[0].style.backgroundColor = "pink";
+	document.getElementById("text-indicator").innerHTML = "Instagram";
+})
+
+insta.addEventListener("mouseout", function() {
+	document.getElementsByTagName("body")[0].style.backgroundColor = "#e2eee4";
+	document.getElementById("text-indicator").innerHTML = "";
+})
+
+git.addEventListener("mouseover", function() {
+	document.getElementsByTagName("body")[0].style.backgroundColor = "grey";
+	document.getElementById("text-indicator").innerHTML = "Github";
+})
+
+git.addEventListener("mouseout", function() {
+	document.getElementsByTagName("body")[0].style.backgroundColor = "#e2eee4";
+	document.getElementById("text-indicator").innerHTML = "";
+})
+
+wp.addEventListener("mouseover", function() {
+	document.getElementsByTagName("body")[0].style.backgroundColor = "green";
+	document.getElementById("text-indicator").innerHTML = "Whastapp";
+})
+
+wp.addEventListener("mouseout", function() {
+	document.getElementsByTagName("body")[0].style.backgroundColor = "#e2eee4";
+	document.getElementById("text-indicator").innerHTML = "";
+})
+
+
